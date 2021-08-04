5c6f5afff020d164a94b41330f7d8538f0b4d49c
n001ce <a1ccormier@gmail.com>
2021-08-04 17:12:29 -0400

    lab complete

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import {Component} from 'react'
+import Giphy from './Components/Giphy.js'
+
+class App extends Component{
+  
+  constructor(props){
+    super(props)
+    this.state= {
+        baseUrl: 'https://api.giphy.com/v1/gifs/search?',
+        apikey: 'api_key=' + process.env.REACT_APP_APIKEY,
+        q: '&q=',
+        queryTitle: '',
+        limit: '&limit=20',
+        offset: '&offset=0',
+        searchUrl: '',
+    }
+  }
+  handleChange = (event) =>{
+      this.setState({[event.target.id]: event.target.value})
+  }
+  handleSubmit = (event) =>{
+      event.preventDefault()
+      this.setState({
+        searchUrl: this.state.baseUrl + this.state.apikey + this.state.q + this.state.queryTitle + this.state.limit
+      }, ()=>{
+        fetch(this.state.searchUrl)
+          .then(response=>{
+             return response.json()
+          }).then(json=> this.setState({
+            giph: json,
+            queryTitle: '',
+          }),
+          err => console.log(err))
+      })
+  }
+
+
+render(){
+  return(
+    <div className="App">
+      <h1 className="header">Giphy Search</h1>
+      <form onSubmit={this.handleSubmit}>
+          <label htmlFor = 'queryTitle'>Search</label>
+          <input
+              id= "queryTitle"
+              type="text"
+              value={this.state.queryTitle}
+              onChange={this.handleChange}
+          />
+          <input 
+              type="submit"
+              value="Find Giph"
+          />
+      </form>
+      {(this.state.giph)
+        ? <Giphy giph={this.state.giph} />
+        : ""
+      }
+    </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Components/Giphy.js
@@ -0,0 +1,15 @@
+function Giphy(props) {
+    return(
+        <div className = "giph-result">
+    {props.giph.data.map((data, indx) => {
+        return (
+            <div className="giph-box" >
+                <img className="media-img card-img-top card-img-hero" src={data.images.downsized.url} alt="Alt text"></img>
+            </div>
+            );
+    })}
+    </div>
+    )
+}
+           
+export default Giphy;
